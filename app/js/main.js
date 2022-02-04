49903be75ba3475a1e60e859421aaffbeb838bca
max4-dev <maksim.bogomyakov@gmail.com>
2022-02-04 21:48:46 +0300

    added header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-  
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+    /*Если сделали скролл на 100px задаём новый класс для header*/
+    if (height > 70) {
+      $('.header').addClass('header-fixed');
+    } else {
+      /*Если меньше 100px удаляем класс для header*/
+      $('.header').removeClass('header-fixed');
+    }
+  });
 })
